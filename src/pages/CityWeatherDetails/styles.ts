71169d6a122865aa9e745b0929f2d9b0fa4bcf19
Andrey Hantya <andriy.hantya@branderstudio.com>
2023-05-14 14:25:18 +0200

    refactor

--- src/pages/CityWeatherDetails/styles.ts
+++ src/pages/CityWeatherDetails/styles.ts
@@ -1,44 +1,44 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 export const DetailsWrapper = styled.div`
-    width: 500px;
-    padding: 0 0 20px;
-    margin: 0 auto;
-    border-radius: 5px;
-    font-family: 'Arial', sans-serif;
-`;
+  width: 500px;
+  padding: 0 0 20px;
+  margin: 0 auto;
+  border-radius: 5px;
+  font-family: 'Arial', sans-serif;
+`
 
 export const Title = styled.h1`
-    padding: 5px;
-    border-radius: 5px;
-    background-color: #4b4b4b;
-    color: #fff;
-    text-align: center;
-`;
+  padding: 5px;
+  border-radius: 5px;
+  background-color: #4b4b4b;
+  color: #fff;
+  text-align: center;
+`
 
 export const InformationWrapper = styled.div`
-    padding-bottom: 10px;
-    border-radius: 5px;
-    background-color: #c1c1c1;
-    color: #000;
-    text-align: center;
-    font-family: 'Arial', sans-serif;
-    font-weight: bold;
-`;
+  padding-bottom: 10px;
+  border-radius: 5px;
+  background-color: #c1c1c1;
+  color: #000;
+  text-align: center;
+  font-family: 'Arial', sans-serif;
+  font-weight: bold;
+`
 
 export const HomeLink = styled(Link)`
-    padding: 15px 30px;
-    margin: 10px auto;
-    border-radius: 5px;
-    display: block;
-    background-color: #4a8748;
-    color: #fff;
-    text-align: center;
-    font-family: 'Arial', sans-serif;
-    font-weight: bold;
-    text-decoration: none;
-    &:hover {
-        background-color: #11660b;
-    }
-`;
+  padding: 15px 30px;
+  margin: 10px auto;
+  border-radius: 5px;
+  display: block;
+  background-color: #4a8749;
+  color: #fff;
+  text-align: center;
+  font-family: 'Arial', sans-serif;
+  font-weight: bold;
+  text-decoration: none;
+  &:hover {
+    background-color: #11660b;
+  }
+`
